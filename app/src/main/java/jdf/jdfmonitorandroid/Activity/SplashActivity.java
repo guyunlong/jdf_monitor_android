bc29a3ca52d1ffcbc75e398968107f85ef295dc2
gylmy <807510889@qq.com>
2018-03-04 16:26:03 +0800

    移除加载页面标题栏

--- app/src/main/java/jdf/jdfmonitorandroid/Activity/SplashActivity.java
+++ app/src/main/java/jdf/jdfmonitorandroid/Activity/SplashActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.view.WindowManager;
 
 import jdf.jdfmonitorandroid.Manager.MyContext;
 import jdf.jdfmonitorandroid.R;
@@ -13,6 +14,8 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash);
         handler.postDelayed(runnable,1000);
     }
